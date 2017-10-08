904a1759639439f0f083a25dee03c62ae300edfb
bannzai <kingkong999yhirose@gmail.com>
2017-10-08 21:41:16 +0900

    :recycle: xcodeproject functions

--- Tests/XcodeProjectTests/XcodeProjectTests.swift
+++ Tests/XcodeProjectTests/XcodeProjectTests.swift
@@ -2,15 +2,45 @@ import XCTest
 @testable import XcodeProject
 
 class XcodeProjectTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        
-    }
-
-
     static var allTests = [
-        ("testExample", testExample),
+        ("testReadToWriteEqualToSameContent", testReadToWriteEqualToSameContent),
     ]
+    
+    private var xcodeProjectUrl: URL {
+        guard
+            let testPath = ProcessInfo().environment["PBXProjectPath"],
+            let url = URL(string: "file://" + testPath)
+            else {
+                XCTFail("Should set environment PBXProjectPath.")
+                fatalError()
+        }
+        
+        return url
+    }
+}
+
+extension XcodeProjectTests {
+    func testRead() {
+        do {
+            let project = try XcodeProject(for: xcodeProjectUrl)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testReadToWriteEqualToSameContent() {
+        do {
+            let originalContent = try String(contentsOf: xcodeProjectUrl, encoding: String.Encoding.utf8)
+            
+            let project = try XcodeProject(for: xcodeProjectUrl)
+            let serialization = XcodeSerialization(project: project)
+            let generateString = try serialization.generateWriteContent()
+            
+            if originalContent != generateString {
+                XCTFail("unexception should same read and write content")
+            }
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
 }
