9058ffdd2c69b56e2e7ff0bd038a5c838e4d7a70
bannzai <kingkong999yhirose@gmail.com>
2017-10-08 20:30:02 +0900

    :metal: add array extension test

--- Tests/XcodeProjectTests/ExtensionTests.swift
+++ Tests/XcodeProjectTests/ExtensionTests.swift
@@ -1,14 +1,38 @@
 import XCTest
 @testable import XcodeProject
 
-class StringExtensionTests: XCTestCase {
-    func testMatchFirst() {
-        XCTAssertEqual(XcodeProject().text, "Hello, World!")
+class ArrayExtensionTests: XCTestCase {
+    func testOfType() {
+        let array: [Any] = [1, 2, 3, "test"]
+        let converted = array.ofType(Int.self)
+        XCTAssert(converted.count == 3)
     }
     
+    func testGroupBy() {
+        struct PBXTest {
+            let isa: String
+            let testId: Int
+        }
+        let array = [
+            PBXTest(isa: "PBXObject", testId: 1),
+            PBXTest(isa: "PBXObject", testId: 2),
+            PBXTest(isa: "PBXObject", testId: 3),
+            PBXTest(isa: "PBXObject", testId: 4),
+            PBXTest(isa: "PBXBuildFile", testId: 1),
+        ]
+        let groupedDictionary = array
+            .groupBy { (object) -> String in
+                return object.isa
+        }
+        
+        XCTAssert(groupedDictionary.keys.count == 2, "Grouped key type is PBXObject, PBXBuildFile")
+        XCTAssert(groupedDictionary["PBXObject"]?.count == 4)
+        XCTAssert(groupedDictionary["PBXBuildFile"]?.count == 1)
+    }
     
     static var allTests = [
-        ("testExample", testExample),
+        ("testOfType", testOfType),
+        ("testGroupBy", testGroupBy),
         ]
 }
 
