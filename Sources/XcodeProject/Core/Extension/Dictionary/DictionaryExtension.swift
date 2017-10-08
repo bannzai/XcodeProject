b457ff506e5f71da0ac4b6b91b1060f8a21fa74f
bannzai <kingkong999yhirose@gmail.com>
2017-10-08 21:18:23 +0900

    :recycle: remove unused function

--- Sources/XcodeProject/Core/Extension/Dictionary/DictionaryExtension.swift
+++ Sources/XcodeProject/Core/Extension/Dictionary/DictionaryExtension.swift
@@ -14,21 +14,3 @@ func += <K, V> (left: inout [K: V], right: [K: V]) {
     }
 }
 
-extension Dictionary {
-    typealias Group = [Key: [Value]]
-    func groupBy() -> [Group] {
-        return reduce([Group]()) { result, pair in
-            let groupedValues = result.flatMap { $0[pair.key] }.first
-            let appendGroup: Group
-            
-            switch groupedValues {
-            case .none:
-                appendGroup = [pair.key: [pair.value]]
-            case .some(let groupedValues):
-                appendGroup = [pair.key: groupedValues + [pair.value]]
-            }
-            return result + [appendGroup]
-        }
-    }
-}
-
