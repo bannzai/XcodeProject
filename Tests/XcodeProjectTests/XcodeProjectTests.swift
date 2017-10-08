442d43c0229d671532d6832be53b28bc3c5d8979
bannzai <kingkong999yhirose@gmail.com>
2017-10-08 21:31:20 +0900

    :recycle: remove unused statement

--- Tests/XcodeProjectTests/XcodeProjectTests.swift
+++ Tests/XcodeProjectTests/XcodeProjectTests.swift
@@ -6,7 +6,7 @@ class XcodeProjectTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(XcodeProject().text, "Hello, World!")
+        
     }
 
 
