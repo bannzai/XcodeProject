7948367f75325a9a1fe4d557e4f7b29cfea09414
bannzai <kingkong999yhirose@gmail.com>
2017-10-08 15:16:02 +0900

    :recycle: remove unused functions

--- Sources/XcodeProject/Core/Component/Environment.swift
+++ Sources/XcodeProject/Core/Component/Environment.swift
@@ -8,34 +8,6 @@
 
 import Foundation
 
-private extension String {
-    func matchFirst(from pattern: String) -> String {
-        guard
-            let regex = try? NSRegularExpression(pattern: pattern, options: [.useUnixLineSeparators, .caseInsensitive])
-            else {
-                fatalError(assertionMessage(description: "unexpected pattern: \(pattern)"))
-        }
-        guard
-            let result = regex.firstMatch(in: self, options: [], range: NSMakeRange(0, self.characters.count))
-            else {
-                return self
-        }
-        
-        let matchingString = (self as NSString).substring(with: result.range(at: 1)) as String
-        
-        // FIXME: why last is whitespace??
-        if matchingString.characters.last == " " {
-            return String(matchingString.characters.dropLast())
-        }
-
-        return matchingString
-    }
-    
-    func ignoreCornerSpace() -> String {
-        return self.matchFirst(from: "^\\s*(.+)\\s*$")
-    }
-}
-
 public enum Environment: String {
     case PROJECT_FILE_PATH
     case TARGET_NAME
@@ -45,25 +17,6 @@ public enum Environment: String {
     case SDKROOT
     case SOURCE_ROOT
     case SRCROOT
-    
-    public static func convert(from content: String) -> [String: String] {
-        let environments = content.components(separatedBy: "\n")
-        var dictionary: [String: String] = [:]
-        environments.forEach { env in
-            let classes = env.components(separatedBy: "=")
-            switch classes.first {
-            case .none:
-                return 
-            case .some(let left):
-                let right = classes.count == 2 ? classes[1] : ""
-                let key = left.ignoreCornerSpace()
-                let value = right.ignoreCornerSpace()
-                
-                dictionary[key] = value
-            }
-        }
-        return dictionary
-    }
     
     fileprivate static var elements: [Environment] {
         return [
