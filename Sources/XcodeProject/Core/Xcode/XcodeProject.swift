b566167a189e818ff513a3aef2da3ddff272462e
bannzai <kingkong999yhirose@gmail.com>
2018-01-03 14:15:13 +0900

    :metal: add resource build phase

--- Sources/XcodeProject/Core/Xcode/XcodeProject.swift
+++ Sources/XcodeProject/Core/Xcode/XcodeProject.swift
@@ -35,14 +35,187 @@ open class XcodeProject {
     }
 }
 
+// MARK: - Append
 extension XcodeProject {
     private func generateHashId() -> String {
-        let all = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".characters.map { String($0) }
+        let all = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toArray().map { String($0) }
         var result: String = ""
         for _ in 0..<24 {
             result += all[Int(arc4random_uniform(UInt32(all.count)))]
         }
         return result
+    }
+    
+    fileprivate func appendFileRef(_ fileName: String, and fileRefId: String) {
+        let fileRef: PBX.FileReference
+        
+        let isa = ObjectType.PBXFileReference.rawValue
+        let pair: PBXPair = [
+            "isa": isa,
+            "fileEncoding": 4,
+            "lastKnownFileType": LastKnownFileType(fileName: fileName).value,
+            "path": fileName,
+            "sourceTree": "<group>"
+        ]
+        
+        fileRef = PBX.FileReference(
+            id: fileRefId,
+            dictionary: pair,
+            isa: isa,
+            allPBX: allPBX
+        )
+        
+        allPBX.dictionary[fileRefId] = fileRef
+    }
+    
+    private func makeGroupEachPaths(for projectRootPath: String) -> [(PBX.Group, String)] {
+        return allPBX
+            .dictionary
+            .values
+            .flatMap {
+                $0 as? PBX.Group
+            }
+            .flatMap { group -> (PBX.Group, String) in
+                let path = (projectRootPath + group.fullPath)
+                    .components(separatedBy: "/")
+                    .filter { !$0.isEmpty }
+                    .joined(separator: "/")
+                return (group, path)
+        }
+    }
+    
+    func appendGroupIfNeeded(with groupEachPaths: [(PBX.Group, String)], childId: String, groupPathNames: [String]) {
+        if groupPathNames.isEmpty {
+            return
+        }
+        
+        let groupPath = groupPathNames.joined(separator: "/")
+        let alreadyExistsGroup = groupEachPaths
+            .filter { (group, path) -> Bool in
+                return path == groupPath
+            }
+            .first
+        
+        if let group = alreadyExistsGroup?.0 {
+            let reference: PBX.Reference = allPBX.dictionary[childId] as! PBX.Reference
+            group.children.append(reference)
+            return
+        } else {
+            guard let pathName = groupPathNames.last else {
+                fatalError("unexpected not exists last value")
+            }
+            
+            let isa = ObjectType.PBXGroup.rawValue
+            let pair: PBXPair = [
+                "isa": isa,
+                "children": [
+                    childId
+                ],
+                "path": pathName,
+                "sourceTree": "<group>"
+            ]
+            
+            let uuid = generateHashId()
+            let group = PBX.Group(
+                id: uuid,
+                dictionary: pair,
+                isa: isa,
+                allPBX: allPBX
+            )
+            
+            allPBX.dictionary[uuid] = group
+            appendGroupIfNeeded(with: groupEachPaths, childId: uuid, groupPathNames: Array(groupPathNames.dropLast()))
+        }
+    }
+    
+    private func makeBuildFile(for buildFileId: String, and fileRefId: String) -> PBX.BuildFile { // build file
+        let isa = ObjectType.PBXBuildFile.rawValue
+        let pair: PBXPair = [
+            "isa": isa,
+            "fileRef": fileRefId,
+            ]
+        
+        let buildFile = PBX.BuildFile(
+            id: buildFileId,
+            dictionary: pair,
+            isa: isa,
+            allPBX: allPBX
+        )
+        allPBX.dictionary[buildFileId] = buildFile
+        
+        return buildFile
+    }
+    
+    private func appendBuildPhase(with buildPhaseId: String, and buildFile: PBX.BuildFile, for targetName: String, fileName: String) {
+        guard let target = allPBX
+            .dictionary
+            .values
+            .flatMap ({ $0 as? PBX.NativeTarget })
+            .filter ({ $0.name == targetName })
+            .first
+            else {
+                fatalError(assertionMessage(description: "Unexpected target name \(targetName)"))
+        }
+        
+        let lastKnownType = LastKnownFileType(fileName: fileName)
+        switch lastKnownType {
+        case .file:
+            appendResourceBuildPhase(with: buildPhaseId, and: buildFile, target: target)
+        case .sourceCode:
+            appendSourceBuildPhase(with: buildPhaseId, and: buildFile, target: target)
+        }
+    }
+    
+    private func appendSourceBuildPhase(with buildPhaseId: String, and buildFile: PBX.BuildFile, target: PBX.NativeTarget) {
+        let sourcesBuildPhase = target.buildPhases.flatMap { $0 as? PBX.SourcesBuildPhase }.first
+        guard sourcesBuildPhase == nil else {
+            // already exists
+            sourcesBuildPhase?.files.append(buildFile)
+            return
+        }
+        
+        let isa = ObjectType.PBXSourcesBuildPhase.rawValue
+        let pair: PBXPair = [
+            "isa": isa,
+            "buildActionMask": Int32.max,
+            "files": [
+                buildFile.id
+            ],
+            "runOnlyForDeploymentPostprocessing": 0
+        ]
+        
+        allPBX.dictionary[buildPhaseId] = PBX.SourcesBuildPhase(
+            id: buildPhaseId,
+            dictionary: pair,
+            isa: isa,
+            allPBX: allPBX
+        )
+    }
+    
+    private func appendResourceBuildPhase(with buildPhaseId: String, and buildFile: PBX.BuildFile, target: PBX.NativeTarget) {
+        let builPhase = target.buildPhases.flatMap { $0 as? PBX.ResourcesBuildPhase }.first
+        guard builPhase == nil else {
+            // already exists
+            builPhase?.files.append(buildFile)
+            return
+        }
+        
+        let isa = ObjectType.PBXResourcesBuildPhase.rawValue
+        let pair: PBXPair = [
+            "isa": isa,
+            "buildActionMask": Int32.max,
+            "files": [
+                buildFile.id
+            ],
+            "runOnlyForDeploymentPostprocessing": 0
+        ]
+        
+        allPBX.dictionary[buildPhaseId] = PBX.ResourcesBuildPhase(
+            id: buildPhaseId,
+            dictionary: pair,
+            isa: isa,
+            allPBX: allPBX
+        )
     }
     
     public func appendFilePath(
@@ -59,151 +232,18 @@ extension XcodeProject {
         }
         
         let fileRefId = generateHashId()
-        let fileRef: PBX.FileReference
-        do { // file ref
-            let isa = ObjectType.PBXFileReference.rawValue
-            let pair: PBXPair = [
-                "isa": isa,
-                "fileEncoding": 4,
-                "lastKnownFileType": LastKnownFileType(fileName: fileName).value,
-                "path": fileName,
-                "sourceTree": "<group>"
-            ]
-            
-            fileRef = PBX.FileReference(
-                id: fileRefId,
-                dictionary: pair,
-                isa: isa,
-                allPBX: allPBX
-            )
-            
-            allPBX.dictionary[fileRefId] = fileRef
-        }
+        appendFileRef(fileName, and: fileRefId)
         
-        do { // groups
-            allPBX.resetFullFilePaths(with: project)
-            
-            let groupEachPaths = allPBX
-                .dictionary
-                .values
-                .flatMap {
-                    $0 as? PBX.Group
-                }
-                .flatMap { group -> (PBX.Group, String) in
-                    let path = (projectRootPath + group.fullPath)
-                        .components(separatedBy: "/")
-                        .filter { !$0.isEmpty }
-                        .joined(separator: "/")
-                    return (group, path)
-            }
-            
-            func appendGroupIfNeeded(with childId: String, groupPathNames: [String]) {
-                if groupPathNames.isEmpty {
-                    return
-                }
-                
-                let groupPath = groupPathNames.joined(separator: "/")
-                let alreadyExistsGroup = groupEachPaths
-                    .filter { (group, path) -> Bool in
-                        return path == groupPath
-                    }
-                    .first
-                
-                if let group = alreadyExistsGroup?.0 {
-                    let reference: PBX.Reference = allPBX.dictionary[childId] as! PBX.Reference
-                    group.children.append(reference)
-                    return
-                } else {
-                    guard let pathName = groupPathNames.last else {
-                        fatalError("unexpected not exists last value")
-                    }
-                    
-                    let isa = ObjectType.PBXGroup.rawValue
-                    let pair: PBXPair = [
-                        "isa": isa,
-                        "children": [
-                            childId
-                        ],
-                        "path": pathName,
-                        "sourceTree": "<group>"
-                    ]
-                    
-                    let uuid = generateHashId()
-                    let group = PBX.Group(
-                        id: uuid,
-                        dictionary: pair,
-                        isa: isa,
-                        allPBX: allPBX
-                    )
-                    
-                    allPBX.dictionary[uuid] = group
-                    appendGroupIfNeeded(with: uuid, groupPathNames: Array(groupPathNames.dropLast()))
-                }
-            }
-            
-            appendGroupIfNeeded(with: fileRefId, groupPathNames: groupPathNames)
-        }
+        allPBX.resetFullFilePaths(with: project)
+        
+        let groupEachPaths = makeGroupEachPaths(for: projectRootPath)
+        appendGroupIfNeeded(with: groupEachPaths, childId: fileRefId, groupPathNames: groupPathNames)
         
         let buildFileId = generateHashId()
-        func buildFile() -> PBX.BuildFile { // build file
-            let isa = ObjectType.PBXBuildFile.rawValue
-            let pair: PBXPair = [
-                "isa": isa,
-                "fileRef": fileRefId,
-            ]
-            
-            let buildFile = PBX.BuildFile(
-                id: buildFileId,
-                dictionary: pair,
-                isa: isa,
-                allPBX: allPBX
-            )
-            allPBX.dictionary[buildFileId] = buildFile 
-            
-            return buildFile 
-        }
+        let buildFile = makeBuildFile(for: buildFileId, and: fileRefId)
         
-        let buildedFile = buildFile()
-        
-        let sourceBuildPhaseId = generateHashId()
-        func sourceBuildPhase() { // source build phase
-            
-            guard let target = allPBX
-                .dictionary
-                .values
-                .flatMap ({ $0 as? PBX.NativeTarget })
-                .filter ({ $0.name == targetName })
-                .first
-                else {
-                    fatalError(assertionMessage(description: "Unexpected target name \(targetName)"))
-            }
-            
-            let sourcesBuildPhase = target.buildPhases.flatMap { $0 as? PBX.SourcesBuildPhase }.first
-            guard sourcesBuildPhase == nil else {
-                // already exists
-                sourcesBuildPhase?.files.append(buildedFile)
-                return
-            }
-            
-            let isa = ObjectType.PBXSourcesBuildPhase.rawValue
-            let pair: PBXPair = [
-                "isa": isa,
-                "buildActionMask": Int32.max,
-                "files": [
-                    buildFileId
-                ],
-                "runOnlyForDeploymentPostprocessing": 0
-            ]
-            
-            allPBX.dictionary[sourceBuildPhaseId] = PBX.SourcesBuildPhase(
-                id: sourceBuildPhaseId,
-                dictionary: pair,
-                isa: isa,
-                allPBX: allPBX
-            )
-        }
-        
-        sourceBuildPhase()
+        let buildPhaseId = generateHashId()
+        appendBuildPhase(with: buildPhaseId, and: buildFile, for: targetName, fileName: fileName)
     }
 }
 
