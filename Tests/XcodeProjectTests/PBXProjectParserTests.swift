80cae44a58b236cfcdba680e2b44bad1373eb478
bannzai <kingkong999yhirose@gmail.com>
2019-07-10 09:10:54 +0900

    :recycle: Move to parser

--- Tests/XcodeProjectTests/PBXProjectParserTests.swift
+++ Tests/XcodeProjectTests/PBXProjectParserTests.swift
@@ -21,4 +21,14 @@ class PBXProjectParserTests: XCTestCase {
         }
     }
 
+    func testShouldCacheContext() {
+        do {
+            let parser = try PBXProjectParser(xcodeprojectUrl: xcodeProjectUrl())
+            let context1 = parser.context()
+            let context2 = parser.context()
+            XCTAssert(context1 === context2)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
 }
--- Tests/XcodeProjectTests/XcodeProjectTests.swift
+++ Tests/XcodeProjectTests/XcodeProjectTests.swift
@@ -3,15 +3,5 @@ import XCTest
 
 class XcodeProjectTests: XCTestCase {
 
-    func testShouldCacheContext() {
-        do {
-            let parser = try PBXProjectParser(xcodeprojectUrl: xcodeProjectUrl())
-            let context1 = parser.context()
-            let context2 = parser.context()
-            XCTAssert(context1 === context2)
-        } catch {
-            XCTFail(error.localizedDescription)
-        }
-    }
-    
+
 }
