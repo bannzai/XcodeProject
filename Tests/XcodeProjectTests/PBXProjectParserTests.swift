846e638df449ae34160188b2c1e18b7771158f2c
bannzai <kingkong999yhirose@gmail.com>
2019-07-10 09:09:33 +0900

    :recycle: Remove unnecessary code

--- Tests/XcodeProjectTests/PBXProjectParserTests.swift
+++ Tests/XcodeProjectTests/PBXProjectParserTests.swift
@@ -9,15 +9,6 @@ import XCTest
 @testable import XcodeProject
 
 class PBXProjectParserTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testParse() {
         do {
             let parser = try PBXProjectParser(xcodeprojectUrl: xcodeProjectUrl())
@@ -27,13 +18,6 @@ class PBXProjectParserTests: XCTestCase {
             XCTAssert(parser.pair().count == 5)
         } catch {
             XCTFail(error.localizedDescription)
-        }
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
 
