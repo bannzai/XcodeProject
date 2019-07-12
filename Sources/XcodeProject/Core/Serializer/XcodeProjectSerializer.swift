48c88440218cab43c09e7c38bfa1de6e3f1e4c26
bannzai <kingkong999yhirose@gmail.com>
2019-07-13 01:31:53 +0900

    :memo: skip if isa
    But I don't understand this condition.

--- Sources/XcodeProject/Core/Serializer/XcodeProjectSerializer.swift
+++ Sources/XcodeProject/Core/Serializer/XcodeProjectSerializer.swift
@@ -222,6 +222,11 @@ internal extension XcodeProjectSerializer {
                 let objectPair = object.objectDictionary
                     .sorted { $0.0 < $1.0 }
                     .compactMap { (key: String, value: Any) -> String? in
+                        if key == "isa" {
+                            // skip
+                            return nil
+                        }
+                        
                         let content = pairString(for: (key, value), with: isa, and: 3)
                         if content.isEmpty {
                             return nil
