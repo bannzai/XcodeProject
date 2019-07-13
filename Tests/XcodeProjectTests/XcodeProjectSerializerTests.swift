04c4228b1a59d6ae1ca414886b7280dd1624875d
bannzai <kingkong999yhirose@gmail.com>
2019-07-13 10:33:15 +0900

    :sparkles: Add test for multiple line

--- Tests/XcodeProjectTests/XcodeProjectSerializerTests.swift
+++ Tests/XcodeProjectTests/XcodeProjectSerializerTests.swift
@@ -27,39 +27,85 @@ class XcodeProjectSerializerTests: XCTestCase {
     }
     
     func testGenerateContentEachSection() {
-        do {
-            let parser = try PBXProjectParser(xcodeprojectUrl: xcodeProjectUrl())
-            let project = XcodeProject(
-                parser: parser,
-                hashIDGenerator: PBXObjectHashIDGenerator()
-            )
-            let serialization = XcodeProjectSerializer(project: project)
-            let got = serialization.generateContentEachSection(
-                isa: .PBXBuildFile,
-                objects: [
-                    PBX.BuildFile.init(
-                        id: "BA4268041F89EB7F001FA700",
-                        dictionary: [
-                            "isa": "PBXBuildFile",
-                            "fileRef": "BA4268031F89EB7F001FA700",
-                        ],
-                        isa: "PBXBuildFile",
-                        allPBX: parser.context()
-                    )
-                ]
-            )
-            XCTAssertEqual(
-                got,
-                """
-/* Begin PBXBuildFile section */
-\(indent)\(indent)BA4268041F89EB7F001FA700 /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = BA4268031F89EB7F001FA700 /* AppDelegate.swift */; };
-/* End PBXBuildFile section */
-
-"""
-            )
-        } catch {
-            XCTFail(error.localizedDescription)
+        XCTContext.runActivity(named: "Single line") { (activity) in
+            do {
+                let parser = try PBXProjectParser(xcodeprojectUrl: xcodeProjectUrl())
+                let project = XcodeProject(
+                    parser: parser,
+                    hashIDGenerator: PBXObjectHashIDGenerator()
+                )
+                let serialization = XcodeProjectSerializer(project: project)
+                let got = serialization.generateContentEachSection(
+                    isa: .PBXBuildFile,
+                    objects: [
+                        PBX.BuildFile.init(
+                            id: "BA4268041F89EB7F001FA700",
+                            dictionary: [
+                                "isa": "PBXBuildFile",
+                                "fileRef": "BA4268031F89EB7F001FA700",
+                            ],
+                            isa: "PBXBuildFile",
+                            allPBX: parser.context()
+                        )
+                    ]
+                )
+                XCTAssertEqual(
+                    got,
+                    """
+                    /* Begin PBXBuildFile section */
+                    \(indent)\(indent)BA4268041F89EB7F001FA700 /* AppDelegate.swift in Sources */ = {isa = PBXBuildFile; fileRef = BA4268031F89EB7F001FA700 /* AppDelegate.swift */; };
+                    /* End PBXBuildFile section */
+                    
+                    """
+                )
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
         }
+        
+        XCTContext.runActivity(named: "Multiple line") { (activity) in
+            do {
+                let parser = try PBXProjectParser(xcodeprojectUrl: xcodeProjectUrl())
+                let project = XcodeProject(
+                    parser: parser,
+                    hashIDGenerator: PBXObjectHashIDGenerator()
+                )
+                let serialization = XcodeProjectSerializer(project: project)
+                let got = serialization.generateContentEachSection(
+                    isa: .PBXContainerItemProxy,
+                    objects: [
+                        PBX.ContainerItemProxy.init(
+                            id: "BA4268151F89EB7F001FA700",
+                            dictionary: [
+                                "isa": "PBXContainerItemProxy",
+                                "containerPortal": "BA4267F81F89EB7F001FA700",
+                                "proxyType": "1",
+                                "remoteGlobalIDString": "BA4267FF1F89EB7F001FA700",
+                                "remoteInfo": "iOSTestProject"
+                            ],
+                            isa: "PBXContainerItemProxy",
+                            allPBX: parser.context()
+                        )
+                    ]
+                )
+                XCTAssertEqual(
+                    got,
+                    """
+                    /* Begin PBXContainerItemProxy section */
+                    \(indent)\(indent)BA4268151F89EB7F001FA700 /* PBXContainerItemProxy */ = {
+                    \(indent)\(indent)\(indent)isa = PBXContainerItemProxy;
+                    \(indent)\(indent)\(indent)containerPortal = BA4267F81F89EB7F001FA700 /* Project object */;
+                    \(indent)\(indent)\(indent)proxyType = 1;
+                    \(indent)\(indent)\(indent)remoteGlobalIDString = BA4267FF1F89EB7F001FA700;
+                    \(indent)\(indent)\(indent)remoteInfo = iOSTestProject;
+                    \(indent)\(indent)};
+                    /* End PBXContainerItemProxy section */
 
+                    """
+                )
+            } catch {
+                XCTFail(error.localizedDescription)
+            }
+        }
     }
 }
