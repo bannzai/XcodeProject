b4a112f3e655ab4a99fd9dc653fc5ef4ab4489e8
bannzai <kingkong999yhirose@gmail.com>
2019-07-16 18:22:04 +0900

    :shell: swift package resolve

--- Package.swift
+++ Package.swift
@@ -20,9 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XcodeProject",
-            dependencies: []),
+            dependencies: ["Swdifft"]),
         .testTarget(
             name: "XcodeProjectTests",
-            dependencies: ["XcodeProject"]),
+            dependencies: ["XcodeProject", "Swdifft"]),
     ]
 )
