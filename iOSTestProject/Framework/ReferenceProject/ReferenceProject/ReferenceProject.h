e4b0fbd7191bb8ba4ee2086267a5f1338830dfc9
bannzai <kingkong999yhirose@gmail.com>
2019-07-20 23:40:56 +0900

    :sparkles: add reference project

---
+++ iOSTestProject/Framework/ReferenceProject/ReferenceProject/ReferenceProject.h
@@ -0,0 +1,19 @@
+//
+//  ReferenceProject.h
+//  ReferenceProject
+//
+//  Created by Yudai.Hirose on 2019/07/20.
+//  Copyright © 2019 bannzai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReferenceProject.
+FOUNDATION_EXPORT double ReferenceProjectVersionNumber;
+
+//! Project version string for ReferenceProject.
+FOUNDATION_EXPORT const unsigned char ReferenceProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReferenceProject/PublicHeader.h>
+
+
