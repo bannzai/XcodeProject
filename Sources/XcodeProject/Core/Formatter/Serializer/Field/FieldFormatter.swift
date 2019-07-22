0e2fbc02a417655ca23a52c206bf29388bfc46a8
bannzai <kingkong999yhirose@gmail.com>
2019-07-22 17:17:19 +0900

    :sparkles: Using maplist formatter

--- Sources/XcodeProject/Core/Formatter/Serializer/Field/FieldFormatter.swift
+++ Sources/XcodeProject/Core/Formatter/Serializer/Field/FieldFormatter.swift
@@ -16,12 +16,15 @@ public struct FieldListFormatterImpl: FieldFormatter {
 
     public let project: XcodeProject
     private let valueListFormatter: AtomicValueListFieldFormatter
+    private let mapListFormatter: PBXRawMapListFormatter
     public init(
         project: XcodeProject,
-        valueListFormatter: AtomicValueListFieldFormatter
+        valueListFormatter: AtomicValueListFieldFormatter,
+        mapListFormatter: PBXRawMapListFormatter
         ) {
         self.project = project
         self.valueListFormatter = valueListFormatter
+        self.mapListFormatter = mapListFormatter
     }
     public func format(of info: FieldFormatterInfomation, for level: Int) -> String {
         let key = try! escape(with: info.key)
@@ -34,6 +37,8 @@ public struct FieldListFormatterImpl: FieldFormatter {
         switch object {
         case let objectIds as [String]:
             return valueListFormatter.format(of: (key: key, value: objectIds, isa: info.isa), for: level)
+        case let mapList as [PBXRawMapType]:
+            return mapListFormatter.format(of: (key: key, value: mapList, isa: info.isa), in: level, next: self)
         case _:
             fatalError("Not implement")
         }
