9d44a30b337329d682d224df1d18263eb869f7bc
bannzai <kingkong999yhirose@gmail.com>
2019-07-22 22:04:39 +0900

    :memo: Remove old test

--- Tests/XcodeProjectTests/XcodeProjectSerializerTests.swift
+++ Tests/XcodeProjectTests/XcodeProjectSerializerTests.swift
@@ -28,20 +28,4 @@ class XcodeProjectSerializerTests: XCTestCase {
             XCTFail(error.localizedDescription)
         }
     }
-    
-    func testEscapeWithTarget() {
-        XCTContext.runActivity(named: "A", block: { _ in
-            do {
-                let (_, serialization) = make()
-                
-                let input = "\"# Type a script or drag a script file from your workspace to insert its path.\necho \"Script\"\n\""
-                let expected = "\"\\\"# Type a script or drag a script file from your workspace to insert its path.\\necho \\\"Script\\\"\\n\\\"\""
-                let got = try serialization.escape(with: input)
-                
-                XCTAssertEqual(got, expected, formatDiff(got, expected))
-            } catch {
-                XCTFail(error.localizedDescription)
-            }
-        })
-    }
 }
