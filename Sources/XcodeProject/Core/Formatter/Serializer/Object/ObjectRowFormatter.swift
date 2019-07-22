20d048e24828a9c80a188e5b25eac9e4610dc090
bannzai <kingkong999yhirose@gmail.com>
2019-07-22 22:51:48 +0900

    :sparkles: Add Sources/XcodeProject/Core/Formatter/Serializer/Object/ObjectRowFormatter

---
+++ Sources/XcodeProject/Core/Formatter/Serializer/Object/ObjectRowFormatter.swift
@@ -0,0 +1,43 @@
+//
+//  ObjectRowFormatter.swift
+//  XcodeProjectTests
+//
+//  Created by Yudai Hirose on 2019/07/22.
+//
+
+import Foundation
+
+public protocol ObjectRowFormatter {
+    func format(context: Context) -> String
+}
+
+
+public struct ObjectRowFormatterImpl: ObjectRowFormatter {
+    private let sectionFormatter: SectionFormatter
+    public init(sectionFormatter: SectionFormatter) {
+       self.sectionFormatter = sectionFormatter
+    }
+    
+    public func format(context: Context) -> String {
+        let groupedObject = context.dictionary
+            .values
+            .toArray()
+            .groupBy { $0.isa.rawValue }
+        let objectsContent = groupedObject
+            .keys
+            .sorted()
+            .map { isa in
+                return (ObjectType(for: isa), groupedObject[isa]!)
+            }
+            .map { isa, objects -> String in
+                return sectionFormatter.format(isa: isa, objects: objects)
+            }
+            .joined(separator: newLine)
+        return """
+        \(indent)objects = {
+        
+        \(objectsContent)\(indent)};
+        
+        """
+    }
+}
