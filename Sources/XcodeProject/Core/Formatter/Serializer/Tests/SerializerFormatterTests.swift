e417308d9ed2570003de576ccbd730551f75ebef
bannzai <kingkong999yhirose@gmail.com>
2019-07-22 21:32:26 +0900

    :sparkles: :construction: Add file for SerializeFormatter Test

---
+++ Sources/XcodeProject/Core/Formatter/Serializer/Tests/SerializerFormatterTests.swift
@@ -0,0 +1,36 @@
+//
+//  SerializerFormatterTests.swift
+//  XcodeProjectTests
+//
+//  Created by Yudai Hirose on 2019/07/22.
+//
+
+import XCTest
+@testable import XcodeProject
+
+class SerializerFormatterTests: XCTestCase {
+    
+    func testEscape() {
+//        XCTContext.runActivity(named: "A", block: { _ in
+//            do {
+//                let (_, serialization) = make()
+//
+//                let input = "\"# Type a script or drag a script file from your workspace to insert its path.\necho \"Script\"\n\""
+//                let expected = "\"\\\"# Type a script or drag a script file from your workspace to insert its path.\\necho \\\"Script\\\"\\n\\\"\""
+//                let got = try serialization.escape(with: input)
+//
+//                XCTAssertEqual(got, expected, formatDiff(got, expected))
+//            } catch {
+//                XCTFail(error.localizedDescription)
+//            }
+//        })
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
