1e91b73b4ddd1db33fbf403dbb513f02e14a03fc
bannzai <kingkong999yhirose@gmail.com>
2019-07-22 22:57:30 +0900

    :sparkles: add top row formatter

---
+++ Sources/XcodeProject/Core/Formatter/Serializer/Object/TopRowFormatter.swift
@@ -0,0 +1,32 @@
+//
+//  TopRowFormatter.swift
+//  Swdifft
+//
+//  Created by Yudai Hirose on 2019/07/22.
+//
+
+import Foundation
+
+public protocol TopRowFormatter {
+    func format(key: PBXRawKeyType) -> String
+}
+
+public struct TopRowFormatterImpl: SerializeFormatter, TopRowFormatter {
+    public let project: XcodeProject
+    public init(project: XcodeProject) {
+        self.project = project
+    }
+    
+    public func format(key: PBXRawKeyType) -> String {
+        let comment = wrapComment(for: key)
+        let row = "\(key) = \(project.fullPair[key]!)\(comment);"
+        let content = row
+            .components(separatedBy: newLine)
+            .map { r in
+                return FormatterIndent.indent + r
+            }
+            .joined(separator: newLine)
+        
+        return content + newLine
+    }
+}
