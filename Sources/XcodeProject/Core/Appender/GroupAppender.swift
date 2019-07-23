157a03de140d37faea91f44bdff2e629378cd1a9
bannzai <kingkong999yhirose@gmail.com>
2019-07-23 13:42:46 +0900

    :sparkles: :construction: add group appender

--- Sources/XcodeProject/Core/Appender/GroupAppender.swift
+++ Sources/XcodeProject/Core/Appender/GroupAppender.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+public typealias GroupPathPair = (PBX.Group, String)
+public protocol GroupAppender {
+    func append(
+        context: Context,
+        groupEachPaths: [GroupPathPair],
+        childId: String,
+        groupPathNames: [String]
+    )
+}
+
+public struct GroupAppenderImpl: GroupAppender {
+    private let hashIDGenerator: StringGenerator
+    public init(
+        hashIDGenerator: StringGenerator
+        ) {
+        self.hashIDGenerator = hashIDGenerator
+    }
+    
+    public func append(
+        context: Context,
+        groupEachPaths: [GroupPathPair],
+        childId: String,
+        groupPathNames: [String]
+        ) {
+        if groupPathNames.isEmpty {
+            return
+        }
+        
+        let groupPath = groupPathNames.joined(separator: "/")
+        let alreadyExistsGroup = groupEachPaths
+            .filter { (group, path) -> Bool in
+                return path == groupPath
+            }
+            .first
+        
+        if let group = alreadyExistsGroup?.0 {
+            let reference: PBX.Reference = context.objects[childId] as! PBX.Reference
+            group.children.append(reference)
+            return
+        } else {
+            guard let pathName = groupPathNames.last else {
+                fatalError("unexpected not exists last value")
+            }
+            
+            let isa = ObjectType.PBXGroup.rawValue
+            let pair: PBXRawMapType = [
+                "isa": isa,
+                "children": [
+                    childId
+                ],
+                "path": pathName,
+                "sourceTree": "<group>"
+            ]
+            
+            let uuid = hashIDGenerator.generate()
+            let group = PBX.Group(
+                id: uuid,
+                dictionary: pair,
+                isa: isa,
+                context: context
+            )
+            
+            context.objects[uuid] = group
+            append(
+                context: context,
+                groupEachPaths: groupEachPaths,
+                childId: uuid,
+                groupPathNames: Array(groupPathNames.dropLast())
+            )
+        }
+    }
+}
