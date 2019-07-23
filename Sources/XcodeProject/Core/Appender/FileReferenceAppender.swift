790729854587e249bdb2396f8c023d6f60decb94
bannzai <kingkong999yhirose@gmail.com>
2019-07-24 08:38:53 +0900

    :recycle: Refactor for file reference appender

--- Sources/XcodeProject/Core/Appender/FileReferenceAppender.swift
+++ Sources/XcodeProject/Core/Appender/FileReferenceAppender.swift
@@ -8,14 +8,36 @@
 import Foundation
 
 public protocol FileReferenceAppender {
-    func append(context: Context, fileName: String, and fileRefId: String)
+    func append(context: Context, filePath: PBXRawPathType) -> PBX.FileReference
 }
 
 public struct FileReferenceAppenderImpl: FileReferenceAppender {
-    public init() { }
+    private let hashIDGenerator: StringGenerator
+    private let groupExtractor: GroupExtractor
+    public init(
+        hashIDGenerator: StringGenerator,
+        groupExtractor: GroupExtractor
+        ) {
+        self.hashIDGenerator = hashIDGenerator
+        self.groupExtractor = groupExtractor
+    }
     
-    public func append(context: Context, fileName: String, and fileRefId: String) {
-        let fileRef: PBX.FileReference
+    private func extractFielRef(context: Context, filePath: PBXRawPathType) -> PBX.FileReference? {
+        // TODO: Implement. but very heavy
+        return nil
+    }
+    
+    public func append(context: Context, filePath: PBXRawPathType) -> PBX.FileReference {
+        let pathComponent = filePath.components(separatedBy: "/")
+        guard let fileName = pathComponent.last else {
+            fatalError()
+        }
+
+        if let reference = extractFielRef(context: context, filePath: filePath) {
+            return reference
+        }
+        
+        let fileRefId = hashIDGenerator.generate()
         
         let isa = ObjectType.PBXFileReference.rawValue
         let pair: PBXRawMapType = [
@@ -26,14 +48,25 @@ public struct FileReferenceAppenderImpl: FileReferenceAppender {
             "sourceTree": "<group>"
         ]
         
-        fileRef = PBX.FileReference(
+        let fileRef = PBX.FileReference(
             id: fileRefId,
             dictionary: pair,
             isa: isa,
             context: context
         )
-        
+
         context.objects[fileRefId] = fileRef
+        
+        appendGroupIfExists: do {
+            let groupPath = pathComponent.dropLast().joined(separator: "/")
+            if let lastGroup = groupExtractor.extract(context: context, path: groupPath) {
+                if let reference = context.objects[lastGroup.id] as? PBX.Group {
+                    lastGroup.children.append(reference)
+                }
+            }
+        }
+        
+        return fileRef
     }
 }
 
