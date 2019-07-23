ba90914aba3956355dbb26bb4e933f0f71ac6f6c
bannzai <kingkong999yhirose@gmail.com>
2019-07-23 12:25:39 +0900

    :sparkles: Add file reference appender

---
+++ Sources/XcodeProject/Core/Appender/FileReferenceAppender.swift
@@ -0,0 +1,37 @@
+//
+//  FileReferenceAppender.swift
+//  XcodeProject
+//
+//  Created by Yudai Hirose on 2019/07/23.
+//
+
+import Foundation
+
+public protocol FileReferenceAppender {
+    func append(context: Context, fileName: String, and fileRefId: String)
+}
+
+public struct FileReferenceAppenderImpl: FileReferenceAppender {
+    public func append(context: Context, fileName: String, and fileRefId: String) {
+        let fileRef: PBX.FileReference
+        
+        let isa = ObjectType.PBXFileReference.rawValue
+        let pair: PBXRawMapType = [
+            "isa": isa,
+            "fileEncoding": 4,
+            "lastKnownFileType": LastKnownFile(fileName: fileName).value,
+            "path": fileName,
+            "sourceTree": "<group>"
+        ]
+        
+        fileRef = PBX.FileReference(
+            id: fileRefId,
+            dictionary: pair,
+            isa: isa,
+            context: context
+        )
+        
+        context.objects[fileRefId] = fileRef
+    }
+}
+
