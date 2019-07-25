a52ad520767dd6ae5f2545ef8d388c66ccbfb75c
bannzai <kingkong999yhirose@gmail.com>
2019-07-25 12:15:26 +0900

    :sparkles: Add test case of group extractor

---
+++ Sources/XcodeProject/Core/Extractor/Tests/GroupExtractorTests.swift
@@ -0,0 +1,50 @@
+//
+//  GroupExtractorTests.swift
+//  XcodeProjectTests
+//
+//  Created by Yudai Hirose on 2019/07/25.
+//
+
+import XCTest
+@testable import XcodeProject
+
+class GroupExtractorTests: XCTestCase {
+    func make() -> GroupExtractor {
+        return GroupExtractorImpl()
+    }
+
+    
+    func testExtract() {
+        XCTContext.runActivity(named: "When is not exists directory", block: { _ in
+            XCTContext.runActivity(named: "When is Hoge directory. Hoge is not exists", block: { _ in
+                let xcodeProject = makeXcodeProject()
+                let extractor = make()
+                
+                let group = extractor.extract(context: xcodeProject.context, path: "Hoge")
+                
+                XCTAssertNil(group)
+            })
+        })
+        XCTContext.runActivity(named: "When is exists directory", block: { _ in
+            XCTContext.runActivity(named: "When is root directory", block: { _ in
+                let xcodeProject = makeXcodeProject()
+                let extractor = make()
+                
+                let group = extractor.extract(context: xcodeProject.context, path: "")
+                
+                XCTAssertNotNil(group)
+                XCTAssertEqual(group!.path, nil)
+                XCTAssertEqual(group!.name, nil)
+            })
+            XCTContext.runActivity(named: "When is not root directory", block: { _ in
+                let xcodeProject = makeXcodeProject()
+                let extractor = make()
+                
+                let group = extractor.extract(context: xcodeProject.context, path: "iOSTestProject")
+                
+                XCTAssertNotNil(group)
+                XCTAssertEqual(group!.path!, "iOSTestProject")
+            })
+        })
+    }
+}
