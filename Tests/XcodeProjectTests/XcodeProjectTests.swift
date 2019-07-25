68c703569c2cdc92cf8eca9584e2464baa7c5daf
bannzai <kingkong999yhirose@gmail.com>
2019-07-25 16:11:27 +0900

    :recycle: Remove unnecessary method

--- Tests/XcodeProjectTests/XcodeProjectTests.swift
+++ Tests/XcodeProjectTests/XcodeProjectTests.swift
@@ -1,18 +1,6 @@
 import XCTest
 @testable import XcodeProject
 
-func isDirectory(_ dirName: String) -> Bool {
-    let fileManager = FileManager.default
-    var isDir: ObjCBool = false
-    if fileManager.fileExists(atPath: dirName, isDirectory: &isDir) {
-        if isDir.boolValue {
-            return true
-        }
-    }
-    return false
-}
-
-
 class XcodeProjectTests: XCTestCase {
     
     // This test is overwrite actual project.pbxproj file.
