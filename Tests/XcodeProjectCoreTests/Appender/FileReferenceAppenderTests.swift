c98ed0da7a75cc2f50ac04f886c17762ead4567a
bannzai <kingkong999yhirose@gmail.com>
2019-07-26 12:25:52 +0900

    :bug: Add test case for FileReferenceAppender

--- Tests/XcodeProjectCoreTests/Appender/FileReferenceAppenderTests.swift
+++ Tests/XcodeProjectCoreTests/Appender/FileReferenceAppenderTests.swift
@@ -16,15 +16,43 @@ class FileReferenceAppenderTests: XCTestCase {
     
     func testAppend() {
         XCTContext.runActivity(named: "When append file is not exist", block: { _ in
-            let xcodeproject = makeXcodeProject()
-            let appender = make()
-            let originalObjects = xcodeproject.context.objects
-            
-            XCTAssertEqual(originalObjects.keys.count, xcodeproject.context.objects.keys.count)
-            XCTAssertEqual(originalObjects.values.compactMap { $0 as? PBX.FileReference }.count, xcodeproject.context.objects.values.compactMap { $0 as? PBX.FileReference }.count)
-            appender.append(context: xcodeproject.context, filePath: "aaaaa.swift")
-            XCTAssertEqual(originalObjects.keys.count + 1, xcodeproject.context.objects.keys.count)
-            XCTAssertEqual(originalObjects.values.compactMap { $0 as? PBX.FileReference }.count + 1, xcodeproject.context.objects.values.compactMap { $0 as? PBX.FileReference }.count)
+            XCTContext.runActivity(named: "To append root direcotroy", block: { _ in
+                let xcodeproject = makeXcodeProject()
+                let appender = make()
+                let originalObjects = xcodeproject.context.objects
+                let originalSubGroupsForMainGroup = xcodeproject.context.mainGroup.subGroups
+                
+                from: do {
+                    XCTAssertEqual(originalObjects.keys.count, xcodeproject.context.objects.keys.count)
+                    XCTAssertEqual(originalObjects.values.compactMap { $0 as? PBX.FileReference }.count, xcodeproject.context.objects.values.compactMap { $0 as? PBX.FileReference }.count)
+                    XCTAssertEqual(originalSubGroupsForMainGroup.count, xcodeproject.context.mainGroup.subGroups.count)
+                }
+                appender.append(context: xcodeproject.context, filePath: "aaaaa.swift")
+                to: do {
+                    XCTAssertEqual(originalObjects.keys.count + 1, xcodeproject.context.objects.keys.count)
+                    XCTAssertEqual(originalObjects.values.compactMap { $0 as? PBX.FileReference }.count + 1, xcodeproject.context.objects.values.compactMap { $0 as? PBX.FileReference }.count)
+                    XCTAssertEqual(originalSubGroupsForMainGroup.count, xcodeproject.context.mainGroup.subGroups.count)
+                }
+            })
+            XCTContext.runActivity(named: "To append iOSTestProject/ direcotroy", block: { _ in
+                let xcodeproject = makeXcodeProject()
+                let appender = make()
+                let originalObjects = xcodeproject.context.objects
+                let originalSubGroups = xcodeproject.groups[path: "iOSTestProject"]!.subGroups
+                
+                from: do {
+                    XCTAssertEqual(originalObjects.keys.count, xcodeproject.context.objects.keys.count)
+                    XCTAssertEqual(originalObjects.values.compactMap { $0 as? PBX.FileReference }.count, xcodeproject.context.objects.values.compactMap { $0 as? PBX.FileReference }.count)
+                    XCTAssertEqual(originalSubGroups.count, xcodeproject.groups[path: "iOSTestProject"]!.subGroups.count)
+                }
+                appender.append(context: xcodeproject.context, filePath: "iOSTsetProject/aaaaa.swift")
+                to: do {
+                    XCTAssertEqual(originalObjects.keys.count + 1, xcodeproject.context.objects.keys.count)
+                    XCTAssertEqual(originalObjects.values.compactMap { $0 as? PBX.FileReference }.count + 1, xcodeproject.context.objects.values.compactMap { $0 as? PBX.FileReference }.count)
+                    XCTAssertEqual(originalSubGroups.count, xcodeproject.groups[path: "iOSTestProject"]!.subGroups.count)
+                }
+            })
+
         })
         
         XCTContext.runActivity(named: "When append file is exist", block: { _ in
