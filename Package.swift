567c94e5006a036f1d0c290be56600d788071dcf
bannzai <kingkong999yhirose@gmail.com>
2019-07-27 02:27:55 +0900

    :memo: Add Ocha package

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/bannzai/Swdifft.git", from: Version(1, 0, 3)),
          .package(url: "https://github.com/jakeheis/SwiftCLI.git", .exact("5.2.2")),
-         .package(url: "https://github.com/kylef/Commander.git", from: Version(0, 8, 0))
+         .package(url: "https://github.com/kylef/Commander.git", from: Version(0, 8, 0)),
+         .package(url: "https://github.com/bannzai/Ocha.git", from: Version(1, 1, 0)),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,6 +31,9 @@ let package = Package(
         .target(
             name: "XcodeProjectCore",
             dependencies: ["Swdifft"]),
+        .target(
+            name: "OchaExample",
+            dependencies: ["XcodeProjectCore"]),
         .testTarget(
             name: "XcodeProjectCoreTests",
             dependencies: ["XcodeProjectCore", "Swdifft"]),
