4cc892f9c3d5ae74b7b83010e0b4a248c06e7d1e
bannzai <kingkong999yhirose@gmail.com>
2019-07-26 18:56:27 +0900

    :memo: Add test file

---
+++ iOSTestProject/iOSTestProject/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  iOSTestProject
+//
+//  Created by Yudai Hirose on 2019/07/26.
+//  Copyright © 2019 廣瀬雄大. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
