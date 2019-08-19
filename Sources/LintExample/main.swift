e12531268dd4667d74924659121c14081783a513
bannzai <kingkong999yhirose@gmail.com>
2019-08-19 15:15:34 +0900

    :sparkles: Fix CLI for lint

--- Sources/LintExample/main.swift
+++ Sources/LintExample/main.swift
@@ -4,12 +4,10 @@ import Foundation
 
 command(
     Argument<String>("pbxproj", description: "Path to project.pbxproj."),
-    Argument<String>("target", description: "Target name for editing project.pbxproj")
-) { (pbxprojPath, targetName) in
-    print("🍵 Start 🍵")
-    let subpaths = try FileManager.default.subpathsOfDirectory(atPath: Process().currentDirectoryPath)
+    Argument<String>("directory", description: "Target directory name for lint project.pbxproj")
+) { (pbxprojPath, directoryName) in
     let xcodeproject = try XcodeProject(xcodeprojectURL: URL(fileURLWithPath: pbxprojPath))
-    RunLoop.current.run()
+    try xcodeproject.sync(from: directoryName)
     }
     .run()
 
