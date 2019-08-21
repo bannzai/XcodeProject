112cc04bf08b30bf9eb2ed620503e351bf04156c
bannzai <kingkong999yhirose@gmail.com>
2019-08-22 08:47:38 +0900

    :memo: Fix specify path

--- scripts/development/generate_mock.sh
+++ scripts/development/generate_mock.sh
@@ -6,4 +6,4 @@ PWD=`dirname $0`
 APP_DIR="$PWD/../../"
 
 cd $APP_DIR
-sourcery --sources ./Sources/XcodeProject/Core --templates ./templates/sourcery/AutoMock.stencil --output ./Tests/XcodeProjectTests/Generated/Mock.generated.swift --args testableImport=XcodeProject
+sourcery --sources ./Sources/XcodeProjectCore --templates ./templates/sourcery/AutoMock.stencil --output ./Tests/XcodeProjectCoreTests/Generated/Mock.generated.swift --args testableImport=XcodeProjectCore
