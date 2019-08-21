74c47e9623e0ce184903563863cc6a1daf24f13b
bannzai <kingkong999yhirose@gmail.com>
2019-08-22 07:59:49 +0900

    :sparkles: add failure test patterng

--- Tests/XcodeProjectCoreTests/XcodeProject/XcodeProjectSyncTests.swift
+++ Tests/XcodeProjectCoreTests/XcodeProject/XcodeProjectSyncTests.swift
@@ -10,12 +10,18 @@ import XCTest
 
 class XcodeProjectSyncTests: XCTestCase {
 
-    func testExpectedFillFullPath() {
+    func testExpectedFileFullPath() {
         XCTContext.runActivity(named: "in group") { (_) in
             let xcodeproject = makeXcodeProject()
             let result = xcodeproject.expectedFileReferenceFullPath(xcodeproject.fileRefs[path: "AppDelegate.swift"]!)
             
             XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/AppDelegate.swift")
+        }
+        XCTContext.runActivity(named: "in group with no reference file") { (_) in
+            let xcodeproject = makeXcodeProject()
+            let result = xcodeproject.expectedFileReferenceFullPath(xcodeproject.fileRefs.filter { $0.name == "NoReferenceFile.swift" }.first!)
+            
+            XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/NoReferenceGroup/NoReferenceFile.swift")
         }
         XCTContext.runActivity(named: "in nested group") { (_) in
             let xcodeproject = makeXcodeProject()
@@ -24,12 +30,40 @@ class XcodeProjectSyncTests: XCTestCase {
             XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/Group/FileReference.swift")
         }
     }
+    
+    func testFileFullPath() {
+        XCTContext.runActivity(named: "in group") { (_) in
+            let xcodeproject = makeXcodeProject()
+            let result = xcodeproject.fileReferenceFullPath(xcodeproject.fileRefs[path: "AppDelegate.swift"]!)
+            
+            XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/AppDelegate.swift")
+        }
+        XCTContext.runActivity(named: "in group with no reference file") { (_) in
+            let xcodeproject = makeXcodeProject()
+            let result = xcodeproject.fileReferenceFullPath(xcodeproject.fileRefs.filter { $0.name == "NoReferenceFile.swift" }.first!)
+            
+            XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/NoReferenceFile.swift")
+        }
+        XCTContext.runActivity(named: "in nested group") { (_) in
+            let xcodeproject = makeXcodeProject()
+            let result = xcodeproject.fileReferenceFullPath(xcodeproject.fileRefs[path: "FileReference.swift"]!)
+            
+            XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/Group/FileReference.swift")
+        }
+    }
+    
     func testExpectedDirectoryFullPath() {
         XCTContext.runActivity(named: "in group") { (_) in
             let xcodeproject = makeXcodeProject()
             let result = xcodeproject.expectedDirectoryFullPath(xcodeproject.groups[fullPath: "iOSTestProject"]!)
             
             XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject")
+        }
+        XCTContext.runActivity(named: "in group with no reference directory") { (_) in
+            let xcodeproject = makeXcodeProject()
+            let result = xcodeproject.expectedDirectoryFullPath(xcodeproject.groups.filter { $0.name == "NoReferenceGroup" }.first!)
+            
+            XCTAssertEqual(result, xcodeproject.context.xcodeprojectDirectoryURL.path + "/iOSTestProject/NoReferenceGroup")
         }
         XCTContext.runActivity(named: "in nested group") { (_) in
             let xcodeproject = makeXcodeProject()
