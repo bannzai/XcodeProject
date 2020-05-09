9cb1e2defc65623649fa6793b9eaa155bcfc88b8
bannzai <kingkong999yhirose@gmail.com>
2020-05-09 19:10:05 +0900

    :memo: Remove Core suffix

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(name: "xcp", targets: ["XcodeProject"]),
         .library(
-            name: "XcodeProjectCore",
+            name: "XcodeProject",
             targets: ["XcodeProjectCore"]),
     ],
     dependencies: [
